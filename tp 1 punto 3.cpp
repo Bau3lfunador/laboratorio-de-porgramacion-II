8366779c5fa728ec4566839a7032a63d41ace6cd
Bau3lfunador <bautistagarciabru@gmail.com>
2025-04-29 08:25:20 -0300

    Update tp 1 punto 3.cpp

--- tp 1 punto 3.cpp
+++ tp 1 punto 3.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-void verificarMenoresADiez(int a, int b, int c) {
+void verificarmenoresadiez(int a, int b, int c) {
     if (a < 10 && b < 10 && c < 10) {
         cout << "todos los numeros son menores a diez" << endl;
     }
@@ -10,6 +10,6 @@ int main() {
     int a, b, c;
     cout << "Ingrese tres numeros: ";
     cin >> a >> b >> c;
-    verificarMenoresADiez(a, b, c);
+    verificarmenoresadiez(a, b, c);
     return 0;
 }
